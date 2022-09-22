6553088078549baf644f62e67746f95500e3de1f
Тиунов Георгий <34783tgs@mail.ru>
2022-09-23 00:12:49 +0300

    добавил сложные задачи)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 import java.time.LocalDate;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         leapYearCheck(2024);
         versionSuggestion((byte)1,2021);
         System.out.println("Потребуется дней: "+daysOfDelivery(95));
-        //
+        int[] array={1,2,3,4,5};
+        reverseArray(array);
+        System.out.println(Arrays.toString(array));
+        doubleCheck("aabccddefgghiijjkk");
+        int[] salaryBook=generateRandomArray();
+        System.out.println("Среднее значение: "+averageValue(salaryBook));
     }
 
     private static void leapYearCheck(int year){
@@ -45,5 +51,44 @@ public class Main {
             days=3;
         }
         return days;
+    }
+
+    private static void reverseArray(int[] array){
+        int temp;
+        for(int i=0;i<array.length/2;i++){
+            temp=array[array.length-1-i];
+            array[array.length-1-i]=array[i];
+            array[i]=temp;
+        }
+    }
+    private static void doubleCheck(String symbols){
+        char[] symbolsArray=symbols.toCharArray();
+        for(int i=0;i<symbols.length()-1;i++){
+            if(symbolsArray[i]==symbolsArray[i+1]){
+                System.out.println("Задублирован символ: "+symbolsArray[i]);
+                return;
+            }
+        }
+        System.out.println("Дублей не найдено");
+    }
+    private static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    private static int sumOfArray(int[] arr){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+    private static float averageValue(int[] array){
+        int sum=sumOfArray(array);
+        int length=array.length;
+        return (float) sum/length;
     }
 }
